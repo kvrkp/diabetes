ea22456341229025a7560a49012dbcbc9b0f1556
Mahesh Natarajan <umamags@gmail.com>
2016-08-14 19:49:29 +0530

    removed platforms and plugins

--- platforms/browser/www/js/filters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Filters */
-angular.module('diabetesFilters', []).filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
-  };
-});
--- platforms/ios/www/js/directives.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-var diabetesDirectives = angular.module('diabetesDirectives', []);
-/* Directives */
